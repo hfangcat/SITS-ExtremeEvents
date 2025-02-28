8de539ce8470ad85962e73563984e0ae55e65504
Heng Fang <hfang@kth.se>
2025-02-28 09:54:22 +0100

    augmentation functions for the training

---
+++ datasets/utils.py
@@ -0,0 +1,45 @@
+"""
+Augmentation functions for the training data
+1. Random flip
+2. Fixed rotation
+3. Gaussian blur √
+4. Contrast, brightness, saturation √
+"""
+
+import numpy as np
+import cv2
+
+
+def gauss_noise(img, var=30):
+    row, col, ch = img.shape
+    mean = var
+    sigma = var**0.5
+    gauss = np.random.normal(mean,sigma,(row,col,ch))
+    gauss = gauss.reshape(row,col,ch)
+    gauss = (gauss - np.min(gauss)).astype(np.uint8)
+    return np.clip(img.astype(np.int32) + gauss, 0, 255).astype('uint8')
+
+
+def _blend(img1, img2, alpha):
+    return np.clip(img1 * alpha + (1 - alpha) * img2, 0, 255).astype('uint8')
+
+
+_alpha = np.asarray([0.114, 0.587, 0.299]).reshape((1, 1, 3))
+def _grayscale(img):
+    return np.sum(_alpha * img, axis=2, keepdims=True)
+
+
+def saturation(img, alpha):
+    gs = _grayscale(img)
+    return _blend(img, gs, alpha)
+
+
+def brightness(img, alpha):
+    gs = np.zeros_like(img)
+    return _blend(img, gs, alpha)
+
+
+def contrast(img, alpha):
+    gs = _grayscale(img)
+    gs = np.repeat(gs.mean(), 3)
+    return _blend(img, gs, alpha)
